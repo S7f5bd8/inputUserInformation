ad4f5ee3553dd8f675d277f43fd844b523e5430c
AISW-509-196 <dlekdnd1@naver.com>
2024-05-30 15:18:51 +0900

    직접풀어보기 7-3 완성

--- app/src/main/java/kr/ac/kopo/inputuserinformation/MainActivity.kt
+++ app/src/main/java/kr/ac/kopo/inputuserinformation/MainActivity.kt
@@ -13,8 +13,8 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
-    lateinit var textName : TextView
-    lateinit var textEmail : TextView
+    lateinit var textName : EditText
+    lateinit var textEmail : EditText
     lateinit var btnDlg : Button
     lateinit var dlgEditName : EditText
     lateinit var dlgEditEmail : EditText
@@ -36,12 +36,14 @@ class MainActivity : AppCompatActivity() {
             var dialog = AlertDialog.Builder(this@MainActivity)
             dialog.setTitle("사용자 정보 입력")
             dialog.setIcon(R.drawable.icon1)
+            dlgEditName = dlgView.findViewById(R.id.editName)
+            dlgEditEmail = dlgView.findViewById(R.id.editEmail)
+            dlgEditName.text = textName.text
+            dlgEditEmail.text = textEmail.text
             dialog.setView(dlgView)
             dialog.setPositiveButton("확인"){dialogL, which ->
-                dlgEditName = dlgView.findViewById(R.id.editName)
-                dlgEditEmail = dlgView.findViewById(R.id.editEmail)
-                textName.text = dlgEditName.text.toString()
-                textEmail.text = dlgEditEmail.text.toString()
+                textName.text = dlgEditName.text
+                textEmail.text = dlgEditEmail.text
             }
             dialog.setNegativeButton("취소"){ dialogL, which ->
                 toastView = View.inflate(this@MainActivity, R.layout.toast, null)
